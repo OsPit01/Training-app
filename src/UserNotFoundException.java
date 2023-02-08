21dadebd98d06c4952c71e34d01978e660b79d00
OsPit01 <ticketinlifeagain@gmail.com>
2023-02-08 10:25:05 +0300

    Improve project

---
+++ src/UserNotFoundException.java
@@ -0,0 +1,21 @@
+public class UserNotFoundException extends Exception {
+
+    public UserNotFoundException(String message, Throwable cause, boolean enableSuppression, boolean writableStackTrace) {
+        super(message, cause, enableSuppression, writableStackTrace);
+    }
+
+    public UserNotFoundException(Throwable cause) {
+        super(cause);
+    }
+
+    public UserNotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+
+    public UserNotFoundException(String message) {
+        super(message);
+    }
+
+    public UserNotFoundException() {
+    }
+}
