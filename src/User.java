66f31fc465e30a55676b1ee07765f90854810e5f
OsPit01 <ticketinlifeagain@gmail.com>
2023-02-09 16:51:03 +0300

    Improve project

--- src/User.java
+++ src/User.java
@@ -2,16 +2,36 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class User {
-    String name;
-    String surname;
-    int id;
-    String login;
-    String password;
+    private String name;
+    private String surname;
+    private int id;
+    private String login;
+    private String password;
 
-    public void addUser(String name, String surname, String login, String password) {
+    public User(String name, String surname, String login, String password) {
         this.name = name;
         this.surname = surname;
         this.login = login;
+        this.password = password;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setLogin(String login) {
+        this.login = login;
+    }
+
+    public void setPassword(String password) {
         this.password = password;
     }
 
@@ -34,8 +54,5 @@ public class User {
     public int getId() {
         return id;
     }
-   // public void call() {
-    //    System.out.printf("name: %s; surname: %s; id: %d; login: %s \n",name,surname,id,login,password);
-  //  }
-}
 
+}
