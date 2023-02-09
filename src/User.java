701c50059c305f1dea73595935e06497ef3db9ee
OsPit01 <ticketinlifeagain@gmail.com>
2023-02-09 17:00:25 +0300

    Improve project

--- src/User.java
+++ src/User.java
@@ -7,31 +7,10 @@ public class User {
     private int id;
     private String login;
     private String password;
-
     public User(String name, String surname, String login, String password) {
         this.name = name;
         this.surname = surname;
         this.login = login;
-        this.password = password;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public void setSurname(String surname) {
-        this.surname = surname;
-    }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setLogin(String login) {
-        this.login = login;
-    }
-
-    public void setPassword(String password) {
         this.password = password;
     }
 
