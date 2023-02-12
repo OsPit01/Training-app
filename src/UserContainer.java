34b2031ac265ca070664be2cf5caae2adfda3e3d
OsPit01 <ticketinlifeagain@gmail.com>
2023-02-12 10:51:09 +0300

    Improve project

--- src/UserContainer.java
+++ src/UserContainer.java
@@ -1,10 +1,9 @@
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 public class UserContainer {
 
-    private final  static List<User> users = new ArrayList();
+    private final static List<User> users = new ArrayList();
 
     public static List<User> getInfoUser() {
         return users;
@@ -13,8 +12,7 @@ public class UserContainer {
     public static boolean isUserNameAndPasswordExists(String username, String password) {
         boolean existUser = false;
         int index = users.indexOf(username);
-
-        if(users.get(index +1).toString().contains(password)) {
+        if (users.get(index + 1).toString().contains(password)) {
             existUser = true;
         }
         return existUser;
