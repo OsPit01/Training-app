22f0c6e16e259e7e8c49542f78e944b4277883b6
OsPit01 <ticketinlifeagain@gmail.com>
2023-02-21 10:18:41 +0300

    Improve project new new

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.util.Scanner;
 
 public class Main {
 
-    private final static Scanner scanner = new Scanner(System.in);
+    private  static final Scanner scanner = new Scanner(System.in);
 
     public static void main(String[] args) throws UserNotFoundException {
 
--- src/UserContainer.java
+++ src/UserContainer.java
@@ -3,25 +3,25 @@ import java.util.List;
 
 public class UserContainer {
 
-    private final static List<User> users = new ArrayList();
+    private static final List<User> users = new ArrayList();
 
-    public final static List<User> getUsers() {
+    public static final List<User> getUsers() {
         return users;
     }
 
     public static boolean isUserNameAndPasswordExists(String userName, String password) throws UserNotFoundException {
         boolean existUser = false;
         for (int i = 0; i < users.size(); i++) {
-            if (users.get(i).getUsername().equals(userName)) {
-                int index = i;
-                if (users.get(index).getPassword().equals(password)) {
+            User foundUser = users.get(i);
+            if (foundUser.getUsername().equals(userName)) {
+                if (foundUser.getPassword().equals(password)) {
                     existUser = true;
-                }
-                else {
+                } else {
                     throw new UserNotFoundException("User with such login and password is not found");
                 }
             }
         }
         return existUser;
+
     }
 }
