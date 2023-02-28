79488d521b70155e9da1ae743cfca2d781147e7a
OsPit01 <ticketinlifeagain@gmail.com>
2023-02-28 17:43:56 +0300

    work_with_exception_8

--- src/command/LoginCommand.java
+++ src/command/LoginCommand.java
@@ -2,6 +2,7 @@ package command;
 
 import container.UserContainer;
 import exception.LoginCommandException;
+import exception.UserNotFoundException;
 
 public class LoginCommand {
     public void execute(String userName, String password) throws LoginCommandException {
@@ -10,7 +11,7 @@ public class LoginCommand {
             if (UserContainer.isUserNameAndPasswordExists(userName, password)) {
                 System.out.println("\n" + "Successful entry");
             }
-        } catch (Exception e) {
+        } catch (UserNotFoundException e) {
             System.out.println("user with login = " + " " + userName + " " + "and password = " + " " + password + " " + "not found in the system");
             throw new LoginCommandException("login error", e);
         }
