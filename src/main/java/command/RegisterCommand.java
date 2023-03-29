af65530f6a50105de591c2f5a3d8d7fba3882939
OsPit01 <ticketinlifeagain@gmail.com>
2023-03-29 19:40:33 +0300

    Work with Junit try#5

--- src/main/java/command/RegisterCommand.java
+++ src/main/java/command/RegisterCommand.java
@@ -4,6 +4,7 @@ import container.UserContainer;
 import model.User;
 
 public class RegisterCommand {
+
     public void register(String createUserName, String createPassword, String name, String surname) {
         User user = new User(createUserName, createPassword, name, surname);
         UserContainer.save(user);
