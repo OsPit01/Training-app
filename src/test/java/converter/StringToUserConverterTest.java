85e593fa76d4b7c394da8134688623b713e9a7eb
OsPit01 <ticketinlifeagain@gmail.com>
2023-03-30 19:16:42 +0300

    Work with Junit try#6

--- src/test/java/converter/StringToUserConverterTest.java
+++ src/test/java/converter/StringToUserConverterTest.java
@@ -15,57 +15,43 @@ public class StringToUserConverterTest {
     }
 
     @Test(expected = IllegalArgumentException.class)
-    public void testConvertBlankString() {
-        String actual = " ";
-
-        stringToUserConverter.convert(actual);
-    }
-
-    @Test(expected = IllegalArgumentException.class)
     public void testConvertNullString() {
         stringToUserConverter.convert(null);
     }
 
     @Test
-    public void testConvertUser() {
-        User actual = null;
-        User expected = null;
-
-        assertEquals("can be User: null", expected, actual);
-    }
-
-    @Test
-    public void testConvertWithDash() {
+    public void  testConvertStringWithDash() {
+        User expected = new User("vany-kaka", "dimon-maradona", "Van-Clock-Dimon", "rbIba-gold");
         User actual = stringToUserConverter.convert("vany-kaka=dimon-maradona=Van-Clock-Dimon=rbIba-gold");
 
         assertNotNull(actual);
-        assertEquals("can be name:Van-Clock-Dimon", "Van-Clock-Dimon", actual.getName());
-        assertEquals("can be surname: rbIba-gold", "rbIba-gold", actual.getSurname());
-        assertEquals("can be password: dimon-maradona", "dimon-maradona", actual.getPassword());
-        assertEquals("can be username: vany-kaka", "vany-kaka", actual.getUsername());
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getSurname(), actual.getSurname());
+        assertEquals(expected.getPassword(), actual.getPassword());
+        assertEquals(expected.getUsername(), actual.getUsername());
     }
 
     @Test
     public void testConvertUserOfThreeWords() {
-
+        User expected = new User("serega three zero", "vany short boy", "Van Clock Vandam", "pushkin aleksandr lol");
         User actual = stringToUserConverter.convert("serega three zero=vany short boy=Van Clock Vandam=pushkin aleksandr lol");
 
         assertNotNull(actual);
-        assertEquals("can be: Van Clock Vandam", "Van Clock Vandam", actual.getName());
-        assertEquals("can be:pushkin aleksandr lol", "pushkin aleksandr lol", actual.getSurname());
-        assertEquals("can be:vany short boy", "vany short boy", actual.getPassword());
-        assertEquals("can be: serega three zero", "serega three zero", actual.getUsername());
-
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getSurname(), actual.getSurname());
+        assertEquals(expected.getPassword(), actual.getPassword());
+        assertEquals(expected.getUsername(), actual.getUsername());
     }
 
     @Test
     public void testConvert() {
+        User expected = new User("Oss", "oss_password", "igor", "osipov");
         User actual = stringToUserConverter.convert("Oss=oss_password=igor=osipov");
 
         assertNotNull(actual);
-        assertEquals("can be userName: Oss", "Oss", actual.getUsername());
-        assertEquals("can be password: oss_password", "oss_password", actual.getPassword());
-        assertEquals("can be name: igor", "igor", actual.getName());
-        assertEquals("can be name: osipov", "osipov", actual.getSurname());
+        assertEquals(expected.getUsername(), actual.getUsername());
+        assertEquals(expected.getPassword(), actual.getPassword());
+        assertEquals(expected.getName(), actual.getName());
+        assertEquals(expected.getSurname(), actual.getSurname());
     }
 }
